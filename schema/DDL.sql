7b947a98310bd550d5ad8d48a9f84d5846735b8f
= <sinulinggatwo@gmail.com>
2021-03-20 16:48:12 +0700

    add DDL for province, city, sub_district, and address

--- schema/DDL.sql
+++ schema/DDL.sql
@@ -22,3 +22,58 @@ CREATE TABLE `product` (
   REFERENCES category_product(category_product_id)
   ON UPDATE CASCADE ON DELETE NO ACTION
 );
+
+CREATE TABLE `province` (
+  province_id INT NOT NULL,
+  province VARCHAR(150) NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NULL,
+  PRIMARY KEY(province_id)
+);
+
+CREATE TABLE `city` (
+  city_id INT NOT NULL,
+  province_id INT NULL,
+  city VARCHAR(150) NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NULL,
+  PRIMARY KEY(city_id),
+  CONSTRAINT `city_province_id_foreign` FOREIGN KEY(province_id)
+  REFERENCES province(province_id)
+  ON UPDATE CASCADE ON DELETE NO ACTION
+);
+
+CREATE TABLE `sub_district` (
+  sub_district_id INT NOT NULL,
+  city_id INT NULL,
+  sub_district VARCHAR(150) NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NULL,
+  PRIMARY KEY(sub_district_id),
+  CONSTRAINT `sub_district_city_id_foreign` FOREIGN KEY(city_id)
+  REFERENCES city(city_id)
+  ON UPDATE CASCADE ON DELETE NO ACTION
+);
+
+CREATE TABLE `address` (
+  address_id INT NOT NULL,
+  province_id INT NULL,
+  city_id INT NULL,
+  sub_district_id INT NULL,
+  address VARCHAR(150) NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NULL,
+  PRIMARY KEY(address_id),
+  CONSTRAINT `address_province_id_foreign` FOREIGN KEY(province_id)
+  REFERENCES province(province_id)
+  ON UPDATE CASCADE ON DELETE NO ACTION,
+
+  CONSTRAINT `address_city_id_foreign` FOREIGN KEY(city_id)
+  REFERENCES city(city_id)
+  ON UPDATE CASCADE ON DELETE NO ACTION,
+
+  CONSTRAINT `address_sub_district_id_foreign` FOREIGN KEY(sub_district_id)
+  REFERENCES sub_district(sub_district_id)
+  ON UPDATE CASCADE ON DELETE NO ACTION
+);
+
