25e8b193722ede1a99212d74b7770058f5f92dcb
= <sinulinggatwo@gmail.com>
2021-04-19 16:23:09 +0700

    add endpoint for get province by id

--- controller/route.go
+++ controller/route.go
@@ -7,12 +7,16 @@ import (
 func RunServer() {
 	router := gin.Default()
 
+	// category-products
 	router.GET("/category-products/:categoryProductId", GetCategoryProductById)
 	router.POST("/category-products", CreateCategoryProduct)
 	router.PUT("/category-products/:categoryProductId", UpdateCategoryProductById)
 	router.DELETE("/category-products/:categoryProductId", DeleteCategoryProductById)
 	router.GET("/category-products", GetAllCategoryProduct)
 	router.GET("/category-products/:categoryProductId/products", GetAllProductByCategoryProductId)
+
+	// provinces
+	router.GET("/provinces/:provinceId", GetProvinceById)
 
 	router.Run(":8080")
 }
