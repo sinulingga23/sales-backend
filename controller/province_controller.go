b53019b9bebbc2068769d04f8fa9110cf71f6a46
= <sinulinggatwo@gmail.com>
2021-04-21 15:58:52 +0700

    add delete province handler

--- controller/province_controller.go
+++ controller/province_controller.go
@@ -110,3 +110,54 @@ func CreateProvince(c *gin.Context) {
 	}{http.StatusInternalServerError, "Somethings wrong!"})
 	return
 }
+
+func DeleteProvinceById(c *gin.Context) {
+	provinceId := 0
+
+	provinceId, err := strconv.Atoi(c.Param("provinceId"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, struct {
+			StatusCode	int 	`json:"statusCode"`
+			Message 	string 	`json:"message"`
+			Errors		string 	`json:"errors"`
+		}{http.StatusBadRequest, "Invalid Request", "Bad Request"})
+		return
+	}
+
+	provinceModel := model.Province{}
+	isThere, err := provinceModel.IsProvinceExistsById(provinceId)
+	if err != nil {
+		c.JSON(http.StatusNotFound, struct {
+			StatusCode	int 	`json:"statusCode"`
+			Message		string 	`json:"message"`
+			Errors		string	`json:"errors"`
+		}{http.StatusNotFound, fmt.Sprintf("%s", err), "Not Found"})
+		return
+	}
+
+	if isThere {
+		isDeleted, err := provinceModel.DeleteProvinceById(provinceId)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, struct {
+				StatusCode 	int 	`json:"statusCode"`
+				Message 	string 	`json:"message"`
+				Errors 		string 	`json:"errors"`
+			}{http.StatusInternalServerError, "Somethings wrong!", fmt.Sprintf("%s", err)})
+			return
+		}
+
+		if isDeleted {
+			c.JSON(http.StatusOK, struct {
+				StatusCode	int 	`json:"statusCode"`
+				Message		string 	`json:"message"`
+			}{http.StatusOK, "Success to delete province."})
+			return
+		}
+	}
+
+	c.JSON(http.StatusInternalServerError, struct {
+		StatusCode	int 	`json:"statusCode"`
+		Message 	string 	`json:"message"`
+	}{http.StatusInternalServerError, "Somethings wrong!"})
+	return
+}
