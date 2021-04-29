02b44b22b6f296e57c6a4d6fdfb8f750e6f3923f
= <sinulinggatwo@gmail.com>
2021-04-29 14:59:27 +0700

    paginate the query of method find all product by category product id

--- model/product_model.go
+++ model/product_model.go
@@ -1,5 +1,9 @@
 package model
 
+import (
+	"sales-backend/utility"
+)
+
 type Product struct {
 	ProductId         string  `json:"productId"`
 	CategoryProductId string  `json:"categoryProductId"`
@@ -8,4 +12,36 @@ type Product struct {
 	Price             float64 `json:"price"`
 	Stock             int     `json:"stock"`
 	Audit             Audit   `json:"audit"`
+}
+
+func (p *Product) GetNumberRecords() (int, error) {
+	db, err := utility.ConnectDB()
+	if err != nil {
+		return 0, err
+	}
+	defer db.Close()
+
+	numberRecords := 0
+	err = db.QueryRow("SELECT COUNT(product_id) FROM product").Scan(&numberRecords)
+	if err != nil {
+		return 0, err
+	}
+
+	return numberRecords, nil
+}
+
+func (p *Product) GetNumberRecordsByCategoryProductId(categoryProductId string) (int, error) {
+	db, err := utility.ConnectDB()
+	if err != nil {
+		return 0, err
+	}
+	defer db.Close()
+
+	numberRecords := 0
+	err = db.QueryRow("SELECT COUNT(product_id) FROM product WHERE category_product_id = ?", categoryProductId).Scan(&numberRecords)
+	if err != nil {
+		return 0, err
+	}
+
+	return numberRecords, nil
 }
