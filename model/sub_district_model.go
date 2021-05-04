5e459e60b0225886d3de5d4ef437a1b369179275
= <sinulinggatwo@gmail.com>
2021-05-04 21:47:51 +0700

    adding a method to get number records by city id

--- model/sub_district_model.go
+++ model/sub_district_model.go
@@ -1,8 +1,28 @@
 package model
 
+import (
+	"sales-backend/utility"
+)
+
 type SubDistrict struct {
 	SubDistrictId	int	`json:"subDistrictId"`
 	CityId		int	`json:"cityId"`
 	SubDistrict	string	`json:"subDistrict"`
 	Audit		Audit	`json:"audit"`
+}
+
+func (sD *SubDistrict) GetNumberRecordsByCityId(cityId int) (int, error) {
+	db, err := utility.ConnectDB()
+	if err != nil {
+		return 0, err
+	}
+	defer db.Close()
+
+	numberRecords := 0
+	err = db.QueryRow("SELECT COUNT(sub_district_id) FROM sub_district WHERE city_id = ?", cityId).Scan(&numberRecords)
+	if err != nil {
+		return 0, err
+	}
+
+	return numberRecords, nil
 }
