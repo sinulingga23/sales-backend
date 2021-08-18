7696db96229e083f86d63124d713cae75906b4a9
= <sinulinggatwo@gmail.com>
2021-08-18 22:08:33 +0700

    adding method to check for customer and employee

--- model/customer_model.go
+++ model/customer_model.go
@@ -1,5 +1,14 @@
 package model
 
+import (
+	"errors"
+
+	"sales-backend/utility"
+)
+
+const (
+	CUSTOMER_ROLE = "customer"
+)
 
 type Customer struct {
 	CustomerId 	string 	`json:"customerId"`
@@ -10,4 +19,63 @@ type Customer struct {
 	Email		string	`json:"email"`
 	PhoneNumber 	string 	`json:"phoneNumber"`
 	Audit      	Audit 	`json:"audit"`
+}
+
+func (c *Customer) IsCustomerExsistById(customerId string) (bool, error) {
+	db, err := utility.ConnectDB()
+	if err != nil {
+		return false, errors.New("Somethings wrong!")
+	}
+	defer db.Close()
+
+	tx, err := db.Begin()
+	if err != nil {
+		tx.Rollback()
+		return false, errors.New("Somethings wrong!")
+	}
+
+	checkUserRole := 0
+	err = tx.QueryRow("SELECT COUNT(user_roles_id) FROM users_roles WHERE user_id = ?", customerId).Scan(&checkUserRole)
+	if err != nil {
+		tx.Rollback()
+		return false, errors.New("Somethings wrong!")
+	}
+
+	// the user not have customer role
+	if checkUserRole == 0 {
+		tx.Rollback()
+		return false, nil
+	}
+
+	roleId := 0
+	err = tx.QueryRow("SELECT role_id FROM users_roles WHERE user_id = ?", customerId).Scan(&roleId)
+	if err != nil {
+		tx.Rollback()
+		return false, errors.New("Somethings wrong!")
+	}
+
+	if roleId == 0 {
+		tx.Rollback()
+		return false, errors.New("Somethings wrong!")
+	}
+
+	checkRole := 0
+	role := ""
+	err = tx.QueryRow("SELECT COUNT(role_id), role_name FROM roles WHERE role_id = ?", roleId).Scan(&checkRole, &role)
+	if err != nil {
+		tx.Rollback()
+		return false, errors.New("Somethings wrong!")
+	}
+
+
+	if checkRole != 1 || role != CUSTOMER_ROLE {
+		tx.Rollback()
+		return false, nil
+	}
+
+	if checkRole == 1 && role == CUSTOMER_ROLE {
+		return true, nil
+	}
+
+	return false, errors.New("Somethings wrong!")
 }
--- model/employee_model.go
+++ model/employee_model.go
@@ -1,5 +1,15 @@
 package model
 
+import (
+	"errors"
+
+	"sales-backend/utility"
+)
+
+const (
+	EMPLOYEE_ROLE = "employee"
+)
+
 type Employee struct {
 	EmployeeId	string	`json:"employeeId"`
 	FirstName	string 	`json:"firstName"`
@@ -9,4 +19,60 @@ type Employee struct {
 	Email		string 	`json:"email"`
 	PhoneNumber	string 	`json:"phoneNumber"`
 	Audit       	Audit 	`json:"audit"`
+}
+
+func (e *Employee) IsEmployeeExistsById(employeeId string) (bool, error) {
+	db, err := utility.ConnectDB()
+	if err != nil {
+		return false, err
+	}
+	defer db.Close()
+
+	tx, err := db.Begin()
+	if err != nil {
+		tx.Rollback()
+		return false, errors.New("Somethings wrong!")
+	}
+
+	checkUserRole := 0
+	err = tx.QueryRow("SELECT COUNT(user_roles_id) FROM users_roles WHERE user_id = ?", employeeId).Scan(&checkUserRole)
+	if err != nil {
+		tx.Rollback()
+		return false, errors.New("Somethings wrong!")
+	}
+
+	if checkUserRole == 0 {
+		tx.Rollback()
+		return false, nil
+	}
+
+	roleId := 0
+	err = tx.QueryRow("SELECT role_id FROM users_roles WHERE user_id = ?", employeeId).Scan(&roleId)
+	if err != nil {
+		tx.Rollback()
+		return false, errors.New("Somethings wrong!")
+	}
+
+	if roleId == 0 {
+		return false, nil
+	}
+
+	checkRole := 0
+	role := ""
+	err = tx.QueryRow("SELECT COUNT(role_id), role_name FROM roles WHERE role_id = ?", roleId).Scan(&checkRole, &role)
+	if err != nil {
+		tx.Rollback()
+		return false, errors.New("Somethings wrong!")
+	}
+
+	if checkRole != 1 || role != EMPLOYEE_ROLE {
+		tx.Rollback()
+		return false, nil
+	}
+
+	if checkRole == 1 && role == EMPLOYEE_ROLE {
+		return true, nil
+	}
+
+	return false, errors.New("Somethings wrong!")
 }
